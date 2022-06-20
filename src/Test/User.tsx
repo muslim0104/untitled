48ffbbeb759a9d49611123f9a13addad5ff4a2b4
muslim0104 <muslimmagamadov95123@gmail.com>
2022-06-20 16:26:39 +0300

    Add posts and user component

---
+++ src/Test/User.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export  const User =()=>{
+    return(
+        <div>
+            <h2>User</h2>
+
+
+        </div>
+    )
+}
---
+++ src/User/Post.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export  const Post =()=>{
+    return(
+        <div>
+            <h2>User</h2>
+
+
+        </div>
+    )
+}
